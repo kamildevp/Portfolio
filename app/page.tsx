617278319a51267776d27bbd5197d4db009a7afb
kamildevp <kamil.k52645@gmail.com>
2025-11-13 05:40:12 +0100

    Added header and main html tags

--- app/page.tsx
+++ app/page.tsx
@@ -7,25 +7,27 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="h-full flex flex-col font-sans">
-      <div className="flex flex-row justify-between align-baseline flex-wrap">
-        <div 
-          className="md:flex-1 p-4 order-2 md:order-1 animate-fadeIn opacity-0"
-          style={{animationDelay: "1.2s"}}
-        >
-          <Link href="/resume.pdf" target="_blank" >
-            <Button text="My Resume" />
-          </Link>
-        </div>
-        <div className="order-1 md:order-2 w-full md:w-auto">
-          <NameHeader />
-        </div>
-        <div className="md:flex-1 order-3">
-          <SocialsHeader />
-        </div>
-      </div>
-      <div>
+      <header>
+        <nav className="flex flex-row justify-between align-baseline flex-wrap">
+          <div 
+            className="md:flex-1 p-4 order-2 md:order-1 animate-fadeIn opacity-0"
+            style={{animationDelay: "1.2s"}}
+          >
+            <Link href="/resume.pdf" target="_blank" >
+              <Button text="My Resume" />
+            </Link>
+          </div>
+          <div className="order-1 md:order-2 w-full md:w-auto">
+            <NameHeader />
+          </div>
+          <div className="md:flex-1 order-3">
+            <SocialsHeader />
+          </div>
+        </nav>
+      </header>
+      <main>
         <ProjectsSection />
-      </div>
+      </main>
     </div>
   );
 }
